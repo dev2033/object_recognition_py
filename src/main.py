529c5a9854104972062d18869c76b255175677bb
dev2033 <dev.elop.02@mail.ru>
2021-05-03 16:51:41 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -22,17 +22,17 @@ def object_detection_on_an_image():
         # segment_target_classes=target_class,
 
         # вырезаем объект и сохраняем в отдельный файл
-        # extract_segmented_objects=True,
-        # save_extracted_objects=True,
+        extract_segmented_objects=True,
+        save_extracted_objects=True,
 
         # выходное изображение
         output_image_name="output.jpg"
     )
 
     # считает колличество объектов
-    # print(result[0]["scores"])
-    # objects_count = len(result[0]["scores"])
-    # print(f"Найдено объектов: {objects_count}")
+    print(result[0]["scores"])
+    objects_count = len(result[0]["scores"])
+    print(f"Найдено объектов: {objects_count}")
 
 
 
